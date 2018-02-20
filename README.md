8dd9c0c5f60bb791712bb7628f06dd28a0cc754e
connorjohnlind <connorjohnlind@gmail.com>
2018-02-19 19:26:01 -0800

    Migrated Assets

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [Connorlind.com](http://www.connorlind.com) is a sandbox for my development as a full-stack Javascript engineer. I will implement new technologies here as I learn them.
 
-This website is built with ES6 Javascript, jQuery, and Sass. It is currently hosted as a static website on Amazon S3.
+This website is built with React and Node.
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,40 @@
+import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './normalize.css';
+import './assets/fonts/Raleway/Raleway.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+/** ***NAVIGATION**** */
+$(function() {
+  const homeBottom = $('nav').position().top;
+  const navHeight = $('nav').height();
+
+  function highlightLink(href) {
+    const $anchor = $(`a[href="${href}"]`);
+    $('.active').removeClass('active');
+    $($anchor).addClass('active');
+  }
+
+  $(window).scroll(function () {
+    const pos = $(this).scrollTop();
+
+    // Fixing Navigation Bar
+    if (pos > homeBottom) {
+      $('nav').addClass('fixed');
+    } else {
+      $('nav').removeClass('fixed');
+    }
+
+    // Link Hilighting
+    if (pos < homeBottom) { highlightLink('#home'); }
+    if (pos > $('#about-main').position().top - navHeight) { highlightLink('#about'); }
+    if (pos > $('#portfolio-main').position().top - navHeight) { highlightLink('#portfolio'); }
+    if (pos > $('#contact-main').position().top - navHeight) { highlightLink('#contact'); }
+  });
+});
